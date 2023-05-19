4723995e58496207f9cd03c30413a6e79ba9e8ff
mauriciabad <hello@mauriciabad.com>
2023-05-18 23:37:17 -0600

    Use tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#ff8201",
+      },
+    },
+  },
+  plugins: [],
+};
