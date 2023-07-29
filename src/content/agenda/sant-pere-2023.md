9ec6f15aefb0fc10c836566727884583b36df3f2
mauriciabad <hello@mauriciabad.com>
2023-07-29 11:41:17 +0200

    Improve first post

---
+++ src/content/agenda/sant-pere-2023.md
@@ -0,0 +1,31 @@
+---
+title: Nit jove de Sant Pere 2023
+description: Comença l'estiuu ☀️ i quina millor manera de fer-ho que amb una festassa 🤩🤪😏
+startDate: 2024-07-01 20:30
+image: /events/sant-pere-2023.jpg
+---
+
+## Poster
+
+![Poster nit jove de Sant Pere 2023](/events/sant-pere-2023.jpg)
+
+## Horari
+
+Dissabte 1 de juliol.
+
+- Sopar a partir de les 20:30.
+- Concerts i DJ's a les 22:30.
+
+## Lloc
+
+Parc de l'Arbreda de Begur.
+
+## Artistes
+
+- The Papa's and The Popo's
+- Kharloss Selektah
+- DJ Evelyn
+
+## Entrada
+
+Gratuïta :)
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,10 @@
 import { defineCollection, z } from 'astro:content';
 
 const agenda = defineCollection({
-	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
+		startDate: z
 			.string()
 			.or(z.date())
 			.transform((val) => new Date(val)),
@@ -14,7 +12,7 @@ const agenda = defineCollection({
 			.string()
 			.optional()
 			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
+		image: z.string().optional(),
 	}),
 });
 
