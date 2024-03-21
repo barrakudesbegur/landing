e59d95133d24d6a356adbf4808cae5b66891a267
maui <hello@mauriciabad.com>
2024-03-21 22:33:31 +0100

    Update domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://barrakudesbegur.netlify.app',
+  site: 'https:///barrakudesbegur.org',
   integrations: [mdx(), sitemap(), tailwind()],
 })
