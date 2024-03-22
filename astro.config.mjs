2465fcc92e8a1838655f9bf653fe6058c9c01f30
maui <hello@mauriciabad.com>
2024-03-22 03:22:54 +0100

    fix site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https:///barrakudesbegur.org',
+  site: 'https://barrakudesbegur.org',
   integrations: [mdx(), sitemap(), tailwind()],
 })
