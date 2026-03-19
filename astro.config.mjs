f3e28d45162c6367759a66d3fa5ff940e1eb79fe
maui <hello@mauriciabad.com>
2026-03-19 05:23:49 +0200

    Better contact and header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
+import icon from 'astro-icon'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://barrakudesbegur.org',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), icon()],
   redirects: {
     '/agenda': '/#esdeveniments',
     '/agenda/[...slug]': '/esdeveniments/[...slug]',
